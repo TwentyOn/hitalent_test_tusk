5e2ea18131be976e409d00fe268cdf1c22336cb8
Alexey <aleksey-000@mail.ru>
2026-04-24 20:51:29 +0500

    серверная часть упакована в отдельную директорию

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -9,18 +9,20 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ycdl04q628cgqgrtmd3pju2*bpyq6395sxgpx#%$g2-p=uzu0c'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
