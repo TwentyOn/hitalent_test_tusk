c32b17568d6fc21b4ea35595c1b2a913a47f494b
Alexey <aleksey-000@mail.ru>
2026-04-24 23:31:43 +0500

    подключение postgresql

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -76,13 +76,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'USER': os.getenv('DB_USER'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
+DB_SCHEMA_NAME = os.getenv('DB_SCHEMA')
 
 
 # Password validation
