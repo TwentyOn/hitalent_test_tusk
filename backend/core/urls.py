7b9d5417e64e4a36c21d4716f6d62017452789e7
Alexey <aleksey-000@mail.ru>
2026-04-25 20:33:12 +0500

    добавлеа конечная точка для реализации CRUD-операций с объектом User

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('proxy_api.urls'))
+    path('api/proxy/', include('proxy_api.urls')),
+    path('api/accounts/', include('accounts.urls')),
 ]
