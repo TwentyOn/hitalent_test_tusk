e08e99d6738934a51a43509f6ba579e671eafbf8
Alexey <aleksey-000@mail.ru>
2026-04-26 19:50:19 +0500

    модель пользователя перенесена в отдельное приложение

--- backend/proxy_api/urls.py
+++ backend/proxy_api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import Test
 
 urlpatterns = [
-    # path('test/', )
+    # path('register/', Test.as_view())
 ]
--- backend/proxy_api/views.py
+++ backend/proxy_api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 # Create your views here.
+class Test(APIView):
+    def post(self, request):
+        print(request.data)
+        return Response({'message': 'ok'})
+
+
+
