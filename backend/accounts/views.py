db25f9aea2c2c83a205c23565374297b6f666d8e
Alexey <aleksey-000@mail.ru>
2026-04-28 16:54:55 +0500

    add: конечная точка для запроса данных пользователя

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,21 +1,31 @@
-from django.shortcuts import render
-from rest_framework.generics import GenericAPIView
+from django.shortcuts import render, get_object_or_404
+from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.exceptions import AuthenticationFailed
 
 from .models import User
 from .serializers import UserSerializer
 
 
 # Create your views here.
-class CreateUserView(GenericAPIView):
+class UserView(GenericViewSet):
     queryset = User.objects.filter(is_active=True)
     serializer_class = UserSerializer
 
-    def post(self, request):
+    def create(self, request):
         data = request.data
-        print(data)
         data['activation_key'] = 'test_key'
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk):
+        if not request.user.is_authenticated:
+            raise AuthenticationFailed('Требуется аунтетификация')
+
+        user = get_object_or_404(User, pk=pk)
+        serializer = self.get_serializer(user)
+
+        return Response(serializer.data)
