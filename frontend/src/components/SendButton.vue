4c807ca73934965543f9bf5f35164f42a66616de
Alexey <aleksey-000@mail.ru>
2026-04-29 20:04:32 +0500

    компонент кнопки для отправки данных

---
+++ frontend/src/components/SendButton.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-btn color="success" v-bind:loading="loading" class="mt-2" type="submit" block>
+    {{ btnText }}
+    <v-icon icon="mdi-chevron-right" end></v-icon>
+    </v-btn>
+</template>
+
+<script setup>
+    const props = defineProps(["loading", 'btnText'])
+</script>
