c16971d30602cb188a00e197b4e2c4cbbe427108
Alexey <aleksey-000@mail.ru>
2026-04-30 17:50:57 +0500

    задача на отправку письма с ключем активации

---
+++ backend/accounts/tasks.py
@@ -0,0 +1,14 @@
+from core.celery import app
+from django.core.mail import send_mail
+
+
+@app.task
+def send_activation_key(email, activation_key):
+    send_mail(
+        'Сервис прокси-доступа',
+        'Здравствуйте!\n\n'
+        f'Ваш новый ключ активации: {activation_key}\n\n'
+        'Желаем приятного пользования!',
+        'proxyservice@domen.ru',
+        [email]
+    )
