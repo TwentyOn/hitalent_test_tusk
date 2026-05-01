7bf345486f6d0e0e76e7714c6a7022b5aee74439
Alexey <aleksey-000@mail.ru>
2026-05-01 17:49:31 +0500

    fix: тело письма теперь на английском языке из-за проблем с кодировкой кирилицы

--- backend/accounts/tasks.py
+++ backend/accounts/tasks.py
@@ -1,14 +1,23 @@
 from core.celery import app
 from django.core.mail import send_mail
+from django.conf import settings
 
 
 @app.task
 def send_activation_key(email, activation_key):
+    subject = 'Proxy-access service'
+    body = f"""
+    Greetings!
+    
+    Your new activation key: {activation_key}
+    
+    Enjoy using it!
+    """
+    from_email = settings.DEFAULT_FROM_EMAIL
+    to_emails = [email]
     send_mail(
-        'Сервис прокси-доступа',
-        'Здравствуйте!\n\n'
-        f'Ваш новый ключ активации: {activation_key}\n\n'
-        'Желаем приятного использования!',
-        'proxyservice@domen.ru',
-        [email]
+        subject=subject,
+        message=body,
+        from_email=from_email,
+        recipient_list=to_emails,
     )
