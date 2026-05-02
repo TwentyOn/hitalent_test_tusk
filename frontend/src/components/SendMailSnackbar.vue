5105a11e4b82d101ec11b48083226e6d80c9197e
Alexey <aleksey-000@mail.ru>
2026-05-02 13:33:58 +0500

    компонент для вывода сообщения об отправке письма

---
+++ frontend/src/components/SendMailSnackbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-snackbar color="success" v-model="snackbar" timeout="5000">Письмо с ключом отправлено на почту</v-snackbar>
+</template>
+
+<script setup>
+    const snackbar = defineModel({type: Boolean, default: false})
+</script>
