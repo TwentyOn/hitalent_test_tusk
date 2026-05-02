604804d40967a6972bd405c91502b19c0553b9c9
Alexey <aleksey-000@mail.ru>
2026-05-02 13:39:22 +0500

    fix: добавлен параметр для отключения кнопки, добавлен интерфейс с аннотацией типов (TS)

--- frontend/src/components/SendButton.vue
+++ frontend/src/components/SendButton.vue
@@ -1,10 +1,15 @@
 <template>
-    <v-btn color="success" v-bind:loading="loading" class="mt-2" type="submit" block>
-    {{ btnText }}
-    <v-icon icon="mdi-chevron-right" end></v-icon>
+    <v-btn color="success" v-bind:loading="loading" v-bind:disabled="disabled" class="mt-2" type="submit" block>
+        {{ btnText }}
+        <v-icon icon="mdi-chevron-right" end></v-icon>
     </v-btn>
 </template>
 
-<script setup>
-    const props = defineProps(["loading", 'btnText'])
+<script setup lang="ts">
+    interface Props {
+        btnText: string,
+        loading: boolean,
+        disabled?: boolean
+    }
+    const { disabled = false } = defineProps<Props>()
 </script>
