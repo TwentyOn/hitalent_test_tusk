1ecd887f8cf324f3291eb27e539bae3fa5b7532b
Alexey <aleksey-000@mail.ru>
2026-05-02 13:37:14 +0500

    добавлено отключение кнопки отправки при невалидности данных формы

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -6,25 +6,25 @@
                     Регистрация
                 </v-card-title>
                 <v-card-text>
-                    <v-form v-model="isValidForm" v-on:submit.prevent="handle">
+                    <v-form v-model="formValid" v-on:submit.prevent="handleSubmit">
                         <v-text-field 
                         label="E-mail" 
                         v-on:update:model-value="clear_errors"
-                        v-bind:error-messages="email_error" 
+                        v-bind:error-messages="errors.email" 
                         v-bind:rules="emailRules" 
-                        v-model="userData.email"></v-text-field>
+                        v-model="formData.email"></v-text-field>
                         <v-text-field
                         label="Пароль"
                         v-on:update:model-value="clear_errors"
                         v-bind:rules="passwordRules"
-                        v-bind:error-messages="password_error"
-                        v-model="userData.password"
+                        v-bind:error-messages="errors.password"
+                        v-model="formData.password"
                         type="password"></v-text-field>
                         <v-text-field v-bind:rules="confPasswordRules" label="Подтвердите пароль" type="password"></v-text-field>
-                        <send-button v-bind:loading="loading" btnText="Зарегистрироваться" />
+                        <send-button v-bind:disabled="!formValid" v-bind:loading="loading" btnText="Зарегистрироваться" />
                         <v-divider class="my-7">или</v-divider>
                         <v-btn to="login" class="mt-2" block>Войти</v-btn>
-                        <v-snackbar v-bind:color="snackColor" v-model="snackbar" timeout="5000">Письмо с ключом отправлено на почту</v-snackbar>
+                        <send-mail-snackbar v-model="snackbar"/>
                     </v-form>
                 </v-card-text>
             </v-card>
@@ -33,49 +33,49 @@
 </template>
 
 <script setup>
-    import { ref } from 'vue';
+    import { ref, watch } from 'vue';
+    import { useRouter } from 'vue-router';
     import { useAuthStore } from '../auth.ts';
     import SendButton from '@/components/SendButton.vue';
+    import sendMailSnackbar from '@/components/SendMailSnackbar.vue';
 
-    const email_error = ref('');
-    const password_error = ref('');
+    const router = useRouter();
+    const authStore = useAuthStore();
+
+    const formValid = ref(false);
+    const loading = ref(false);
+    const formData = ref({});
+    const errors = ref({})
 
     const snackbar = ref(false);
-    const snackColor = ref('success');
-
-    const isValidForm = ref(false);
-    const loading = ref(false);
-    const userData = ref({
-        email: "",
-        password: "",
-    });
 
     function clear_errors() {
-        email_error.value = ''
-        password_error.value = ''
+        errors.value = {}
     }
 
-    async function handle() {
-        if (isValidForm.value) {
-            try {
-                const authStore = useAuthStore()
-                loading.value = true;
-                const result = await authStore.register(userData.value);
-                if (result['success']) {
-                    snackbar.value = true;
-                } else {
-                    const errors = result['errors'];
-                    email_error.value = errors.email;
-                    password_error.value = errors.password;
-                }
-            } catch (error) {
-                alert(`Ошибка регистрации: ${error}`)
-            } finally {
-                loading.value = false;
+    async function handleSubmit() {
+        try {
+            loading.value = true
+            const result = await authStore.register(formData.value)
+
+            if (result['success']) {
+                snackbar.value = true;
+            } else {
+                errors.value = result['errors']
             }
-            
+        } catch (error) {
+            alert(`Ошибка регистрации: ${error}`)
+            throw error
+        } finally {
+            loading.value = false
         }
     }
+
+    watch (snackbar, (newVal, oldVal) => {
+        if (oldVal === true && newVal === false) {
+            router.push({'name': 'login'})
+        }
+    })
 
     const emailRules = [
         value => {
@@ -83,22 +83,19 @@
             return "Введите e-mail"
         },
     ]
-
     const passwordRules = [
         value => {
             if (value) return true
             return "Введите пароль"
         },
-
     ]
-
     const confPasswordRules = [
         value => {
             if (value) return true
             return "Подтвердите пароль"
         },
         value => {
-            if (value == userData.value.password) return true
+            if (value == formData.value.password) return true
             return "Пароли не совпадают"
         }
     ]
