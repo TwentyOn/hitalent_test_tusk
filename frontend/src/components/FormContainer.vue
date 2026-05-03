55dc5e453b55517f7e2cff4fee362b292cd5c4f9
Alexey <aleksey-000@mail.ru>
2026-05-03 13:49:35 +0500

    базовый контейнер для форм

---
+++ frontend/src/components/FormContainer.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-container class="fill-height d-flex flex-column justify-center" fluid>
+        <v-sheet width="375" class="border pa-12 mx-auto" rounded="xl">
+            <slot></slot>
+        </v-sheet>
+    </v-container>
+</template>
