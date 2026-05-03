183fdc1a293dfd90475a75895bdf0bb7556908d9
Alexey <aleksey-000@mail.ru>
2026-05-03 20:09:14 +0500

    add: кастомное разрешение для проверки соответствия пользователя

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from .permissions import UserOwnerPermission
+from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
-from rest_framework.exceptions import AuthenticationFailed
 
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, ChangePasswordSerializer
 from .tasks import send_activation_key
 
 
@@ -16,6 +17,11 @@ class UserView(GenericViewSet):
     queryset = User.objects.filter(is_active=True)
     serializer_class = UserSerializer
 
+    def get_permissions(self):
+        if self.action in ('retrieve', 'change_password', 'logout'):
+            return [UserOwnerPermission()]
+        return [permissions.AllowAny()]
+
     def create(self, request):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
@@ -23,17 +29,23 @@ class UserView(GenericViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk):
-        if not request.user.is_authenticated:
-            raise AuthenticationFailed('Требуется аунтетификация')
-
-        user = get_object_or_404(User, pk=pk)
+        user = self.get_object()
         serializer = self.get_serializer(user)
 
         return Response(serializer.data)
 
+    @action(methods=['post'], detail=True, url_path='change-password', serializer_class=ChangePasswordSerializer)
+    def change_password(self, request, pk):
+        user = self.get_object()
+        serializer = self.get_serializer(data=request.data, context={'user': user})
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({'message': 'успех'})
+
 
 class ActivationKeyView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [UserOwnerPermission]
 
     def post(self, request, user_id):
         """
@@ -42,9 +54,11 @@ class ActivationKeyView(APIView):
         :return:
         """
         user = get_object_or_404(User, pk=user_id)
-        user.activation_key = user.create_activation_key()
+
+        activation_key = user.create_activation_key()
+        user.activation_key = activation_key
         user.save()
 
-        send_activation_key.delay(user.email, user.activation_key)
+        send_activation_key.delay(user.email, activation_key)
 
-        return Response({'message': 'успех'})
+        return Response({'activation_key': activation_key})
