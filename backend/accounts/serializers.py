92fcdf83e9279afaa76ab09700eeb2c44a2a8c10
Alexey <aleksey-000@mail.ru>
2026-05-03 15:34:17 +0500

    add: сериализатор для сброса пароля

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['email', 'password', 'activation_key', 'activation_key_expires']
         extra_kwargs = {
-            'activation_key': {'write_only': True},
+            'activation_key': {'read_only': True},
             'password': {'write_only': True}
         }
 
@@ -21,4 +21,32 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
 
+        return user
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True, write_only=True)
+    new_password = serializers.CharField(required=True, write_only=True)
+
+    def validate_old_password(self, old_password):
+        user = self.context.get('user')
+
+        if not user:
+            raise ValueError('Параметр user не передан в контекст')
+
+        if not user.check_password(old_password):
+            raise serializers.ValidationError('Неверный пароль')
+
+    def validate_new_password(self, new_password):
+        validate_password(new_password)
+        return new_password
+
+    def save(self, **kwargs):
+        user = self.context.get('user')
+
+        if not user:
+            raise ValueError('Параметр user не передан в контекст')
+
+        user.set_password(self.validated_data['new_password'])
+        user.save()
         return user
