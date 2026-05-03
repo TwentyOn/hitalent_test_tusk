a9d7c72ea997027bc6080e0d283c8382cc94045f
Alexey <aleksey-000@mail.ru>
2026-05-03 20:10:01 +0500

    add: конечная точка для включения токенов в черный список

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView, TokenBlacklistView
 from rest_framework.routers import DefaultRouter
 
 from .views import UserView, ActivationKeyView
@@ -10,6 +10,7 @@ router.register(prefix='', viewset=UserView)
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/blacklist/', TokenBlacklistView.as_view()),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('<int:user_id>/update-key/', ActivationKeyView.as_view())
 ] + router.urls
