d0111c890faeb67eb45b4ba2d2e249ea741facc2
Alexey <aleksey-000@mail.ru>
2026-05-04 17:29:21 +0500

    валидатор для значений параметра protocol

---
+++ backend/proxy_api/validators.py
@@ -0,0 +1,8 @@
+from django.core.exceptions import ValidationError
+
+
+def protocol_validator(value):
+    allowed_protocols = ('socks5', 'http', 'https')
+    print(value)
+    if value not in allowed_protocols:
+        raise ValidationError(f'Некорректный протокол. Выберите один из: {", ".join(allowed_protocols)}')
