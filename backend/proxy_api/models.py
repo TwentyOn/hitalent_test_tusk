9faf325b5c9f0347ceff54faf8659e54c56c0fdb
Alexey <aleksey-000@mail.ru>
2026-05-04 18:00:57 +0500

    минорная правка

--- backend/proxy_api/models.py
+++ backend/proxy_api/models.py
@@ -16,7 +16,7 @@ class VirtualMachine(models.Model):
     protocol = models.CharField(validators=[protocol_validator])
     is_active = models.BooleanField(default=True)
     current_user = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True)
-    last_user_at = models.DateTimeField(null=True, auto_now=True)
+    last_used_at = models.DateTimeField(null=True, auto_now=True)
 
     def save(self, *args, **kwargs):
         self.full_clean()
