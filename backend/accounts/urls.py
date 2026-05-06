fe67aab26bb79f140b79ba6ff0888ed815ac4a14
Alexey <aleksey-000@mail.ru>
2026-05-06 14:32:11 +0500

    add: имя маршрута

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/blacklist/', TokenBlacklistView.as_view()),
-    path('<int:id>/update-key/', UpdateKeyView.as_view())
+    path('<int:pk>/update-key/', UpdateKeyView.as_view(), name='user-update-key')
 ] + router.urls
