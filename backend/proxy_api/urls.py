60849060e62bf3c5203f7c16fb31767eedbb35a3
Alexey <aleksey-000@mail.ru>
2026-05-06 13:00:56 +0500

    add: имя маршрута activate_key

--- backend/proxy_api/urls.py
+++ backend/proxy_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ActivateKeyView
 
 urlpatterns = [
-    path('activate-key/', ActivateKeyView.as_view())
+    path('activate-key/', ActivateKeyView.as_view(), name='activate-key')
 ]
