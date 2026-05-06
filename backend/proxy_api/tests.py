1494996038bf6304f556a0093d90fef5390b1d88
Alexey <aleksey-000@mail.ru>
2026-05-06 19:21:28 +0500

    правка конфликтов имен (tests<apps) 3

--- backend/proxy_api/tests.py
+++ backend/proxy_api/tests.py
@@ -11,9 +11,9 @@ from proxy_api.models import VirtualMachine
 class ActivateKeyViewTests(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.vm1 = VirtualMachine.objects.create(name='test1', host='203.236.197.76', port=8080, protocol='http')
-        self.vm2 = VirtualMachine.objects.create(name='test2', host='186.144.48.132', port=8080, protocol='https')
-        self.vm3 = VirtualMachine.objects.create(name='test3', host='209.161.114.209', port=8080, protocol='socks5')
+        self.vm1 = VirtualMachine.objects.create(name='test1', host='203.236.453.76', port=8080, protocol='http')
+        self.vm2 = VirtualMachine.objects.create(name='test2', host='186.144.48.235', port=8080, protocol='https')
+        self.vm3 = VirtualMachine.objects.create(name='test3', host='209.235.114.209', port=8080, protocol='socks5')
         self.user = get_user_model().objects.create(email='test@mail.ru', password='09testpassword')
 
     def test_activate_key(self):
