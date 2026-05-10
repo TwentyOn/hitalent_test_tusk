bf60ba6dece21202c881bb6364117f19d568122a
Alexey <aleksey-000@mail.ru>
2026-05-10 13:16:55 +0500

    NGINX: деплой

--- desktop/settings.py
+++ desktop/settings.py
@@ -8,6 +8,6 @@
 # ACTIVATE_KEY_PATH = os.getenv('ACTIVATE_KEY_PATH')
 # WEB_SOCKET_PATH = os.getenv('WEB_SOCKET_PATH')
 
-BASE_URL = 'http://localhost:8000'
+BASE_URL = 'http://localhost'
 ACTIVATE_KEY_PATH = '/api/proxy/activate-key/'
 WEB_SOCKET_PATH = '/ws/connection-status/{user_id}/'
