41893f1c071c4433d95a929643a9c414dbd2f01a
Aleksey <aleksey-000@mail.ru>
2026-07-02 21:31:14 +0500

    update

--- dds/models.py
+++ dds/models.py
@@ -7,7 +7,7 @@ from dictionary.models import Status, Type, Category
 # Create your models here.
 class Dds(models.Model):
     created_at = models.DateField(auto_now_add=True)
-    status = models.ForeignKey(to=Status, on_delete=models.DO_NOTHING, verbose_name='Статус')
+    status = models.ForeignKey(to=Status, on_delete=models.DO_NOTHING, verbose_name='Статус', blank=True, null=True)
     type = models.ForeignKey(to=Type, on_delete=models.PROTECT, verbose_name='Тип')
     category = models.ForeignKey(to=Category, on_delete=models.PROTECT, verbose_name='Категория')
     summa = models.IntegerField(verbose_name='Сумма, руб')
--- static/update-data.js
+++ static/update-data.js
@@ -1,22 +1,29 @@
 const typeField = document.getElementById('typeField')
 const categoryField = document.getElementById('categoryField')
+const subCategoryField = document
 
 typeField.addEventListener('change', typeChange)
 categoryField.innerHTML = '<option value selected>выберите тип</option>'
 
 
 async function typeChange(event) {
-    response = await fetch(`http://127.0.0.1:8000/api/dictionary/type/${typeField.value}/categories/`, {
-        method: 'GET'
-    })
-    if (response.ok) {
-        body = await response.json()
-        categoryField.innerHTML = '<option value selected>---------</option>'
-        for (let key in body) {
-            const option = document.createElement('option')
-            option.value = key
-            option.textContent = body[key]
-            categoryField.appendChild(option)
+    cur_option = typeField.options[typeField.selectedIndex].text
+    if (cur_option !== '---------') {
+        response = await fetch(`http://127.0.0.1:8000/api/dictionary/type/${typeField.value}/categories/`, {
+            method: 'GET'
+        })
+        if (response.ok) {
+            body = await response.json()
+            categoryField.innerHTML = '<option value selected>---------</option>'
+            for (let key in body) {
+                const option = document.createElement('option')
+                option.value = key
+                option.textContent = body[key]
+                categoryField.appendChild(option)
+            }
         }
+    } else {
+        categoryField.innerHTML = '<option value selected>выберите тип</option>'
     }
+
 }
