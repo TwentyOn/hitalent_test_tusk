099c9ecb6ac27240c721dbb6f3d7867467a7257d
Aleksey <aleksey-000@mail.ru>
2026-07-09 21:35:12 +0500

    elastic config

--- app/settings.py
+++ app/settings.py
@@ -15,4 +15,6 @@ class DbConfig(BaseModel):
     def get_db_url(self):
         return f'postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.db_name}'
 
-ELASTIC_API_KEY = os.getenv("ELASTIC_API_KEY")
+class ElasticConfig(BaseModel):
+    host: str = os.getenv("ELASTIC_HOST")
+    api_key: str = os.getenv("ELASTIC_API_KEY")
