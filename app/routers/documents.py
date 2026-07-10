bf3b23fe361afce57ba97398d223fdabf5cc8f21
Aleksey <aleksey-000@mail.ru>
2026-07-10 18:08:07 +0500

    minor fix

--- app/routers/documents.py
+++ app/routers/documents.py
@@ -3,26 +3,23 @@ import logging
 from fastapi import APIRouter, HTTPException, Response, Depends
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from elasticsearch import Elasticsearch, NotFoundError
+from elasticsearch import AsyncElasticsearch, NotFoundError
 
 from backend.db import get_db
 from backend.elastic import get_client, INDEX_NAME
 from models import Document
 from schemas import DocumentSchema
 
-
 logger = logging.getLogger(__name__)
 router = APIRouter(tags=['документы'])
-
 
 
 @router.get('/documents', response_model=list[DocumentSchema], description='Поиск документов')
 async def search_documents(
         query: str,
         db: AsyncSession = Depends(get_db),
-        es: Elasticsearch = Depends(get_client)
+        es: AsyncElasticsearch = Depends(get_client)
 ) -> list[DocumentSchema]:
-
     docs = await es.search(
         index=INDEX_NAME,
         query={
@@ -48,7 +45,7 @@ async def search_documents(
 async def delete_document(
         pk: int,
         db: AsyncSession = Depends(get_db),
-        es: Elasticsearch = Depends(get_client)
+        es: AsyncElasticsearch = Depends(get_client)
 ):
     stmt = select(Document).where(Document.id == pk)
     result = await db.scalars(stmt)
@@ -60,13 +57,12 @@ async def delete_document(
     await db.delete(db_document)
 
     try:
-        await get_client().delete(index=INDEX_NAME, id=str(pk))
+        await es.delete(index=INDEX_NAME, id=str(pk))
     except NotFoundError:
         raise HTTPException(status_code=404, detail='Не удалось найти документ')
     except Exception as err:
         logger.error('Не удалось удалить документ в индексе ElasticSearch', exc_info=True)
         raise HTTPException(status_code=500, detail='Не удалось удалить документ в индексе')
-
 
     await db.commit()
 
