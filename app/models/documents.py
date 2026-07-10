7bb6271acee61657c465a272579faa7ff4ec6fcd
Aleksey <aleksey-000@mail.ru>
2026-07-10 14:28:43 +0500

    fix: конфигурация для тестовой среды

--- app/models/documents.py
+++ app/models/documents.py
@@ -1,6 +1,6 @@
 import datetime
 
-from sqlalchemy import Column, Integer, Text, String, DateTime, ARRAY
+from sqlalchemy import Column, Integer, Text, String, DateTime, ARRAY, JSON
 
 from backend.db import Base
 
@@ -8,6 +8,6 @@ class Document(Base):
     __tablename__ = 'documents'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    rubrics = Column(ARRAY(String))
+    rubrics = Column(ARRAY(String).with_variant(JSON, 'sqlite'))
     text = Column(Text)
     created_date = Column(DateTime, default=datetime.datetime.now)
