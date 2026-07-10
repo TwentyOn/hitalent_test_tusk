55687d066ee37713c8c270001b1b8f61d663ccce
Aleksey <aleksey-000@mail.ru>
2026-07-10 18:08:50 +0500

    minor fix

--- app/backend/elastic.py
+++ app/backend/elastic.py
@@ -1,6 +1,6 @@
 import logging
 
-from elasticsearch import AsyncElasticsearch, NotFoundError
+from elasticsearch import AsyncElasticsearch
 
 from settings import ElasticConfig
 
@@ -9,36 +9,11 @@ config = ElasticConfig()
 INDEX_NAME = 'documents'
 
 client = AsyncElasticsearch(
-        config.host,
-        request_timeout=30,
-        api_key=config.api_key,
-    )
+    config.host,
+    request_timeout=30,
+    api_key=config.api_key,
+)
+
 
 def get_client() -> AsyncElasticsearch:
     return client
-
-
-# async def search_docs(index_name: str, query: str) -> list[int]:
-#     docs = await get_client().search(
-#         index=index_name,
-#         query={
-#             "match": {
-#                 'text': query
-#             }
-#         },
-#         size=20
-#     )
-#     ids = [d['_source']['id'] for d in docs['hits']['hits']]
-#     return ids
-
-
-# async def delete_doc(index_name: str, pk: int) -> bool:
-#     try:
-#         resp = await get_client().delete(index=index_name, id=str(pk))
-#         return True if resp.get('result') == 'deleted' else False
-#     except NotFoundError:
-#         logger.error('Не удалось найти документ в индексе ElasticSearch', exc_info=True)
-#         return False
-#     except Exception as err:
-#         logger.error('Не удалось удалить документ в индексе ElasticSearch', exc_info=True)
-#         return False
