48cb8d207e5eb067b882639d11ec5b3cf0e58f4c
Aleksey <aleksey-000@mail.ru>
2026-07-10 14:29:26 +0500

    fix

--- app/tests/test_delete.py
+++ app/tests/test_delete.py
@@ -1,5 +1,4 @@
 import pytest
-from fastapi.testclient import TestClient
 
 # TODO добавить тестовую БД
 @pytest.mark.parametrize('doc_id, status_code', [
@@ -7,6 +6,5 @@ from fastapi.testclient import TestClient
     (1501, 404)
 ])
 def test_delete(client, doc_id: int, status_code: int):
-
     response = client.delete(f'/documents/{doc_id}/')
     assert response.status_code == status_code
