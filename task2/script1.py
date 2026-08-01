9a8b1b9c6ef2e4920f061768c245f4edd32dd158
Aleksey <aleksey-000@mail.ru>
2026-08-01 19:24:35 +0500

    update

--- task2/script1.py
+++ task2/script1.py
@@ -1,15 +1,16 @@
 import json
 import os
+from pathlib import Path
 from collections import defaultdict
 import shutil
-from string import digits
+
+IMG_PATH = Path('docs/images/train')
+SAVE_PATH = Path('docs/images')
+COCO_FILE_PATH = Path('docs/annotations/instances_train.json')
 
 def script1():
-    img_path = 'docs/images/train'
-    save_path = 'docs/images'
-
-    with open('docs/annotations/instances_train.json') as json_file:
-        coco = json.load(json_file)
+    with open(COCO_FILE_PATH) as coco_json_file:
+        coco = json.load(coco_json_file)
 
     id_to_category = {cat['id']: cat['name'] for cat in coco['categories']}
 
@@ -20,13 +21,15 @@ def script1():
     for img in coco['images']:
         cls = '_'.join(c.split('_')[0] for c in img_to_classes[img['id']])
         img_name = img['file_name']
+        source_path = IMG_PATH / img_name
+        target_path = SAVE_PATH / cls / img_name
+        img['file_name'] = target_path.as_posix()
+        os.makedirs(os.path.join(SAVE_PATH, cls), exist_ok=True)
+        shutil.move(source_path, target_path)
 
-        source_path = os.path.join(img_path, img_name)
-        target_path = os.path.join(save_path, cls, img_name)
-
-        if cls:
-            os.makedirs(os.path.join(save_path, cls), exist_ok=True)
-            shutil.move(source_path, target_path)
+    update_ann_path = COCO_FILE_PATH.parent / 'updated_annotations.json'
+    with open(update_ann_path, 'w') as upd_ann_json_file:
+        json.dump(coco, upd_ann_json_file, indent=4)
 
 
 if __name__ == '__main__':
